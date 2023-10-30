f726a18bc7578cb3dc576ca24fdf299d9f924d81
Erwin Marte <erwin.mdq@gmail.com>
2023-10-29 19:56:40 -1000

    add endpoint or url to mailer router

--- src/routes/emailRouter.js
+++ src/routes/emailRouter.js
@@ -4,6 +4,7 @@ const express = require("express");
 const router = express.Router();
 const { passwordReset, confirmPasswordReset, redirect } = require("../controllers/emailController");
 const { jsonWebTokenVerify } = require("../middleware/authMiddleware");
+const { badMethod } = require("../utils/errorHandler");
 
 // Endpoint for send email the user requested
 router.post("/password", passwordReset);
@@ -11,5 +12,6 @@ router.post("/password", passwordReset);
 router.get("/password/:token", redirect);
 // Change the password endpoint
 router.post("/confirm", jsonWebTokenVerify ,confirmPasswordReset);
+router.all("/", badMethod);
 
 module.exports = router;
--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -4,6 +4,4 @@ function badMethod() {
     return  res.status(405).json({ message: 'Method Not Allowed' });
 };
 
-
-
-module.exports = {badMethod};
+module.exports = { badMethod };
