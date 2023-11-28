7b7ef70e4ef9d4f0ec95286af163788a1dc36d93
erwin <erwin.mdq@gmail.com>
2023-11-29 00:48:28 +0100

    fuck me

--- src/app.js
+++ src/app.js
@@ -32,7 +32,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Cors
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    headers: "Content-Type, Authorization"
+}));
 
 // Logs
 app.use(logGenerator);
